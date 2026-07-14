0dff1247810163926ef8355d318d9cc45ba3f19a
BernardOnuh <onuhbernard4@gmail.com>
2026-07-14 20:37:31 +0100

    update types comment in gamesupabase

--- lib/gameSupabase.ts
+++ lib/gameSupabase.ts
@@ -5,7 +5,7 @@ const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-// Types
+// Type definitions
 export type GameResult = {
     id?: number;
     wallet: string;
